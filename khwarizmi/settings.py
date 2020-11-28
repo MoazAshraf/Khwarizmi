dcd3d6da140cb7608f9ce3e4d1baf376e8d4f678
Moaz Ashraf <mouadhashraf@gmail.com>
2020-11-28 03:52:33 +0200

    Updated profile detail template

--- khwarizmi/settings.py
+++ khwarizmi/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG') == 'True')
 
-ALLOWED_HOSTS = ['khwarizmiapp.herokuapp.com', '.localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['khwarizmiapp.herokuapp.com']
+if DEBUG:
+    ALLOWED_HOSTS += ['.localhost', '127.0.0.1', '[::1]']
 
 # Application definition
 
